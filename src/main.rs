220c7a5e44de77178e0d28246602a4771bc21193
Victor <victor.as.nascimento@gmail.com>
2021-07-12 08:34:06 -0300

    adding get quotes by author features

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,50 @@
-use actix_web::{get, App, Error, HttpResponse, HttpServer};
+extern crate serde;
+extern crate serde_json;
+use actix_web::{web, get, App, Error, HttpResponse, HttpServer};
 use http::StatusCode;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io::Read;
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Quotes {
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct QuotesList {
+    quotes: Vec<Quote>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Quote {
     author: String,
     quote: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct QuotesList {
-    quotes: Vec<Quotes>,
+#[derive(Deserialize)]
+struct AuthorEndpointParams {
+    author: String,
 }
 
-fn _read_quotes_file() -> QuotesList {
-    let mut file = File::open("./quotes.json").unwrap();
-    let mut buff = String::new();
-    file.read_to_string(&mut buff).unwrap();
-
-    serde_json::from_str(&buff).expect("Parsing quotes")
-}
-
-fn read_quotes_file() -> serde_json::Value {
+fn read_quotes_file() -> QuotesList {
     let quotes_json = File::open("./quotes.json").expect("file should open read only");
     serde_json::from_reader(quotes_json).expect("JSON not serializable")
 }
 
 #[get("/quotes/{author}")]
-async fn get_quotes_by_author(author: String) -> Result<HttpResponse, Error> {
-    let quotes_json= _read_quotes_file();
+async fn get_quotes_by_author(info: web::Path<AuthorEndpointParams>) -> Result<HttpResponse, Error> {
+    let quotes_json = read_quotes_file();
+    let quotes_by_author_query_result: QuotesList = QuotesList {
+        quotes: quotes_json
+            .quotes
+            .into_iter()
+            .filter(|quote| quote.author == info.author)
+            .collect::<Vec<Quote>>(),
+    };
 
-    Ok(HttpResponse::Ok().status(StatusCode::OK).json(quotes_json.quotes))
+    Ok(HttpResponse::Ok()
+        .status(StatusCode::OK)
+        .json(quotes_by_author_query_result))
 }
 
 #[get("/quotes")]
 async fn get_quotes() -> Result<HttpResponse, Error> {
     let quotes = read_quotes_file();
-    // println!("{:?}", quotes.as_object().unwrap().get("quotes").unwrap().as_array().unwrap());
 
     Ok(HttpResponse::Ok().status(StatusCode::OK).json(quotes))
 }
