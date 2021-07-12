51d18a7cf4d0c763a899e669e0d9069f9f73eee6
Victor <victor.as.nascimento@gmail.com>
2021-07-12 08:59:15 -0300

    lower case treatment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate serde;
 extern crate serde_json;
-use actix_web::{web, get, App, Error, HttpResponse, HttpServer};
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
 use http::StatusCode;
+use rand::seq::SliceRandom;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 
@@ -26,14 +27,29 @@ fn read_quotes_file() -> QuotesList {
     serde_json::from_reader(quotes_json).expect("JSON not serializable")
 }
 
+#[get("/")]
+async fn get_random_quote() -> Result<HttpResponse, Error> {
+    let quotes_list = read_quotes_file();
+    let random_quote = quotes_list.quotes.choose(&mut rand::thread_rng());
+    if let Some(quote) = random_quote {
+        Ok(HttpResponse::Ok().status(StatusCode::OK).json(quote))
+    } else {
+        Ok(HttpResponse::Ok()
+            .status(StatusCode::OK)
+            .json("Something went wrong"))
+    }
+}
+
 #[get("/quotes/{author}")]
-async fn get_quotes_by_author(info: web::Path<AuthorEndpointParams>) -> Result<HttpResponse, Error> {
-    let quotes_json = read_quotes_file();
+async fn get_quotes_by_author(
+    info: web::Path<AuthorEndpointParams>,
+) -> Result<HttpResponse, Error> {
+    let quotes_list = read_quotes_file();
     let quotes_by_author_query_result: QuotesList = QuotesList {
-        quotes: quotes_json
+        quotes: quotes_list
             .quotes
             .into_iter()
-            .filter(|quote| quote.author == info.author)
+            .filter(|quote|  quote.author.to_ascii_lowercase().contains(&info.author.to_ascii_lowercase()))
             .collect::<Vec<Quote>>(),
     };
 
@@ -44,15 +60,20 @@ async fn get_quotes_by_author(info: web::Path<AuthorEndpointParams>) -> Result<H
 
 #[get("/quotes")]
 async fn get_quotes() -> Result<HttpResponse, Error> {
-    let quotes = read_quotes_file();
+    let quotes_list = read_quotes_file();
 
-    Ok(HttpResponse::Ok().status(StatusCode::OK).json(quotes))
+    Ok(HttpResponse::Ok().status(StatusCode::OK).json(quotes_list))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_quotes).service(get_quotes_by_author))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(get_random_quote)
+            .service(get_quotes)
+            .service(get_quotes_by_author)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
